640d7d760919a5a7112da379d2b1878746760bf9
Joseph W. Becher <drazisil@users.noreply.github.com>
2015-11-07 02:37:45 -0500

    Tests aren't async

--- test/testjpm-report.js
+++ test/testjpm-report.js
@@ -5,12 +5,12 @@ var assert = require('assert')
 var jpm_report = require('../src/index.js')
 
 describe('Checking args', function () {
-  it('with no args', function (done) {
+  it('with no args', function () {
     })
 
-  it('with one arg', function (done) {
+  it('with one arg', function () {
     })
 
-  it('with more then one arg', function (done) {
+  it('with more then one arg', function () {
     })
 })

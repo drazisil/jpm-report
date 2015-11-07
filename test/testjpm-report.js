45771cb5a417f79d9a856f449c2271695eaa9f69
Joseph W. Becher <drazisil@users.noreply.github.com>
2015-11-07 02:40:15 -0500

    Tests should be pending

--- test/testjpm-report.js
+++ test/testjpm-report.js
@@ -5,12 +5,9 @@ var assert = require('assert')
 var jpm_report = require('../src/index.js')
 
 describe('Checking args', function () {
-  it('with no args', function () {
-    })
+  it('with no args')
 
-  it('with one arg', function () {
-    })
+  it('with one arg')
 
-  it('with more then one arg', function () {
-    })
+  it('with more then one arg')
 })

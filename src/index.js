240e43c3cf882b94b0063efbe5509250139528ff
Joseph W Becher <jwbecher@drazisil.com>
2015-11-07 13:18:26 -0500

    Should be working arg processing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+'use strict'
 
+var fs = require('fs')
+var VERSION = require('../package.json').version
+
+function checkArgs (args, cb) {
+  var argCount = args.length
+  if (argCount < 3) {
+    showHelp(cb)
+  } else if (argCount === 3) {
+    parseArg(args[2], cb)
+  } else if (argCount > 3) {
+    cb('ERROR: too many arguments, please use --help')
+  }
+}
+
+function parseArg (arg, cb) {
+  if (arg === '--version') {
+    cb(VERSION)
+  } else {
+    // Check if a file
+    fs.stat(arg, function cb_stat (err, stats) {
+      if (err || !stats.isFile()) {
+        cb('ERROR: ' + arg + ' is not a file.')
+      } else {
+        parseReport(arg, cb)
+      }
+    })
+  }
+}
+
+function parseReport (path, cb) {
+  cb(null, "I'm a file")
+}
+
+function showHelp (cb) {
+  cb('Please use --version')
+}
+
+module.exports.checkArgs = checkArgs
+module.exports.parseArg = parseArg
+module.exports.parseReport = parseReport
+module.exports.showHelp = showHelp
--- test/testjpm-report.js
+++ test/testjpm-report.js
@@ -5,9 +5,42 @@ var assert = require('assert')
 var jpm_report = require('../src/index.js')
 
 describe('Checking args', function () {
-  it('with no args')
+  it('with no args', function cb_parse_args_zero (done) {
+    jpm_report.checkArgs(['node', 'jpm_report'], function cb_parse_args (err, res) {
+      if (err) {
+        assert.equal('Please use --version', err)
+        done()
+      }
+    })
+  })
 
-  it('with one arg')
+  it('with one arg - not a file', function cb_parse_args_one_not (done) {
+    jpm_report.checkArgs(['node', 'jpm_report', 'moo'], function cb_parse_args (err, res) {
+      if (err) {
+        assert.equal('ERROR: moo is not a file.', err)
+        done()
+      }
+    })
+  })
 
-  it('with more then one arg')
+  it('with one arg - a file', function cb_parse_args_one (done) {
+    jpm_report.checkArgs(['node', 'jpm_report', 'bin/jpm-report'], function cb_parse_args (err, res) {
+      if (err) {
+        assert.fail('should not have an error')
+        done()
+      } else {
+        assert.equal("I'm a file", res)
+        done()
+      }
+    })
+  })
+
+  it('with more then one arg', function cb_parse_args_two (done) {
+    jpm_report.checkArgs(['node', 'jpm_report', 'do', 'stuff'], function cb_parse_args (err, res) {
+      if (err) {
+        assert.equal('ERROR: too many arguments, please use --help', err)
+        done()
+      }
+    })
+  })
 })

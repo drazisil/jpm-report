6f1a8fad2f4b9ee7980b50fc42b82052ac2186af
Joseph W. Becher <drazisil@users.noreply.github.com>
2015-11-07 02:31:23 -0500

    Added tests for args

--- test/testjpm-report.js
+++ test/testjpm-report.js
@@ -1,1 +1,19 @@
+'use strict'
 
+/* global describe it */
+var assert = require('assert')
+var jpm_report = require('../lib/index.js')
+
+describe('Checking args', function () {
+  it('with no args', function (done) {
+    })
+  })
+
+  it('with one arg', function (done) {
+    })
+  })
+
+  it('with more then one arg', function (done) {
+    })
+  })
+})

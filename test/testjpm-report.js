f8b47b81d7257d8a248d27f3f3ce38deac14cdca
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-19 06:44:38 -0400

    test cleanup
    [ci skip]

--- test/testjpm-report.js
+++ test/testjpm-report.js
@@ -37,7 +37,7 @@ describe('Checking args', function () {
   it('with one arg - a file', function cb_parse_args_one (done) {
     jpm_report.checkArgs(['node', 'jpm_report', 'test-data/success.txt'], function cb_parse_args (err, res) {
       if (err) {
-        assert.fail('should not have an error')
+        assert.equal('should not have an error', err)
         done()
       } else {
         res = JSON.parse(res)
@@ -47,7 +47,7 @@ describe('Checking args', function () {
     })
   })
 
-  it.skip('with more then one arg', function cb_parse_args_two (done) {
+  it('with more then one arg', function cb_parse_args_two (done) {
     jpm_report.checkArgs(['node', 'jpm_report', 'do', 'stuff'], function cb_parse_args (err, res) {
       if (err) {
         assert.equal('jpm-report <input file> (output file)', err)
@@ -71,7 +71,7 @@ describe('testing report processing, output: foo', function () {
 })
 
 describe('testing report processing, output: json', function () {
-  it.skip('invalid file', function (done) {
+  it('invalid file', function (done) {
     jpm_report.parseReport('test-data/FHFFY^fdhwhd', 'json', function cb_parse_report (err, res) {
       if (err) {
         assert.equal("Error: ENOENT, open 'test-data/FHFFY^fdhwhd'", err)
@@ -108,7 +108,7 @@ describe('testing report processing, output: json', function () {
   it('test failure', function (done) {
     jpm_report.parseReport('test-data/failure.txt', 'json', function cb_parse_report (err, res) {
       if (err) {
-        assert.fail('should not have an error')
+        assert.equal('should not have an error', err)
         done()
       } else {
         res = JSON.parse(res)
@@ -121,7 +121,7 @@ describe('testing report processing, output: json', function () {
   it('test success', function (done) {
     jpm_report.parseReport('test-data/success.txt', 'json', function cb_parse_report (err, res) {
       if (err) {
-        assert.fail('should not have an error')
+        assert.equal('should not have an error', err)
         done()
       } else {
         res = JSON.parse(res)
@@ -163,14 +163,14 @@ describe('testing junit output: success', function () {
   })
 
     it('test fail', function (done) {
-    jpm_report.parseReport('test-data/error.txt', 'json', function cb_parse_report (err, res) {
+    jpm_report.parseReport('test-data/failure.txt', 'json', function cb_parse_report (err, res) {
       if (err) {
         assert.equal('should not have an error', err)
         done()
       } else {
-        fs.writeFileSync('test-data/error.json', res)
-        var inJSON = fs.readFileSync('test-data/error.json')
-        jpm_report.outputJUnit2File(inJSON, 'test-data/error.xml', function cbOutputJUnitFile(exitCode) {
+        fs.writeFileSync('test-data/failure.json', res)
+        var inJSON = fs.readFileSync('test-data/failure.json')
+        jpm_report.outputJUnit2File(inJSON, 'test-data/failure.xml', function cbOutputJUnitFile(exitCode) {
           assert.ok('success')
           done()
         })

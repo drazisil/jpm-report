b9ca10eb952d91ed0e613e1ee28dc0ad354aea83
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-19 15:13:53 -0400

    What about now?

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function parseArg (args, cb) {
     cb(VERSION)
   } else {
     // Check if a file
-    fs.stat(args[0], function cb_stat (err, stats, cb) {
+    fs.stat(args[0], cb, function cb_stat (err, stats) {
       if (err || !stats.isFile()) {
         cb('ERROR: ' + args[0] + ' is not a file.')
       } else {

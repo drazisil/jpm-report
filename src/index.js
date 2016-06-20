592d7076e00f12f235db9c5207f18a1ba46dbf68
Joseph W Becher <jwbecher@drazisil.com>
2016-06-20 03:57:45 -0400

    corrected the bin

--- src/index.js
+++ src/index.js
@@ -82,22 +82,22 @@ function outputJUnit (input, cb) {
   var total_failures = res.success.total_tests - res.success.total_success
   var total_tests = res.success.total_tests
   var output = ''
-  output += '<testsuites>\n'
   output += '<testsuite errors="0" failures="' + total_failures + '"' +
     ' name="' + res.success.package_name + '"' +
     ' timestamp="' + toISOStringJUnit(new Date()) + '"' +
     ' tests="' + total_tests + '"' +
     ' hostname="localhost"' +
     ' time="223">\n'
-  output += '  <properties />\n'
+  output += '  <properties>\n<property name="generator" value="jpm-report" />\n</properties>\n'
   var testcase = '  <testcase classname="main"' +
   ' name="' + res.success.package_name + '"' +
-  ' time="223" />\n'
-  for (var i = 0; i < res.success.total_tests; i++) {
-    output += testcase
-  }
+  ' time="223"></testcase>\n'
+  var system_out = '<system-out />\n'
+  var system_err = '<system-err />\n'
+  output += testcase
+  output += system_out
+  output += system_err
   output += '</testsuite>'
-  output += '</testsuites>'
   cb(null, output)
 }
 
@@ -116,13 +116,11 @@ function outputJUnit2File (input, filename, cb) {
   var testcase = '  <testcase classname="main"' +
   ' name="' + res.success.package_name + '"' +
   ' time="223"></testcase>\n'
-//  for (var i = 0; i < res.success.total_tests; i++) {
   var system_out = '<system-out />\n'
   var system_err = '<system-err />\n'
   output += testcase
   output += system_out
   output += system_err
-//  }
   output += '</testsuite>'
   fs.writeFileSync(filename, output)
   cb(null, 0)
